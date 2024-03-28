18f867ab645695e119d9c608861e3e6452f84433
Vitor Ernane <vitorernane@hotmail.com>
2024-03-28 18:31:23 -0300

    foi adicionado os metodos do ciclo de vida e uma funcao para criar uma view

--- Weather-UIKIT/Weather-UIKIT/ViewController.swift
+++ Weather-UIKIT/Weather-UIKIT/ViewController.swift
@@ -8,13 +8,54 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+// Métodos do Ciclo de Vida
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.backgroundColor = UIColor.red
+        setupView()
     }
 
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+    }
 
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+    }
+    
+    
+    //Métodos
+    
+    private func setupView() {
+        
+        view.backgroundColor = .white
+        
+        let customView = UIView(frame: .zero)
+        customView.backgroundColor = .green
+        customView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(customView)
+        
+        NSLayoutConstraint.activate([
+            customView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 100),
+            
+            customView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 50),
+            
+            customView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -50),
+            
+            customView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -100),
+        ])
+        
+    }
+    
 }
 
