be5d41117a4847998791480aff77d939ba8977a7
Vitor Ernane <vitorernane@hotmail.com>
2024-04-04 23:48:11 -0300

    Foi criado uma stackView no projeto para orientacoes de vento e umidade do WeatherApp

--- Weather-UIKIT/Weather-UIKIT/ViewController.swift
+++ Weather-UIKIT/Weather-UIKIT/ViewController.swift
@@ -63,6 +63,7 @@ class ViewController: UIViewController {
     private lazy var weatherIcon: UIImageView = {
         
         let imageView = UIImageView(frame: .zero)
+        
         imageView.translatesAutoresizingMaskIntoConstraints = false
         imageView.image = UIImage(named: "sunIcon")
         imageView.contentMode = .scaleAspectFit
@@ -70,6 +71,102 @@ class ViewController: UIViewController {
         return imageView
         
     }()
+    
+    private lazy var humidityLabel: UILabel = {
+        
+        let label = UILabel(frame: .zero)
+        
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = UIFont.systemFont(ofSize: 12, weight: .semibold)
+        label.text = "Umidade"
+        label.textColor = UIColor(named: "contrastColor")
+        
+        return label
+    }()
+    
+    private lazy var humidityValueLabel: UILabel = {
+        
+        let label = UILabel(frame: .zero)
+        
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = UIFont.systemFont(ofSize: 12, weight: .semibold)
+        label.text = "1000mm"
+        label.textColor = UIColor(named: "contrastColor")
+        
+        return label
+    }()
+    
+    private lazy var humidityStackView: UIStackView = {
+        
+        let stackView = UIStackView(arrangedSubviews: [humidityLabel, humidityValueLabel])
+        
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .horizontal
+        // stackView.backgroundColor = UIColor.white
+        
+        return stackView
+    }()
+    
+    private lazy var windLabel: UILabel = {
+        
+        let label = UILabel(frame: .zero)
+        
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = UIFont.systemFont(ofSize: 12, weight: .semibold)
+        label.text = "Vento"
+        label.textColor = UIColor(named: "contrastColor")
+        
+        return label
+    }()
+    
+    private lazy var windValueLabel: UILabel = {
+        
+        let label = UILabel(frame: .zero)
+        
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = UIFont.systemFont(ofSize: 12, weight: .semibold)
+        label.text = "10km/h"
+        label.textColor = UIColor(named: "contrastColor")
+        
+        return label
+    }()
+    
+    private lazy var windStackView: UIStackView = {
+        
+        let stackView = UIStackView(arrangedSubviews: [windLabel, windValueLabel])
+        
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .horizontal
+        // stackView.backgroundColor = UIColor.white
+        
+        return stackView
+    }()
+    
+    
+    // Aqui eu estou criando uma stack vier vertical e estou incluindo as duas horizontais dentro dela e depois setando apenas ela dentro da hierarquia.
+     
+    private lazy var statsStackView: UIStackView = {
+        
+        let stackView = UIStackView(arrangedSubviews: [humidityStackView, windStackView])
+        
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .vertical
+        stackView.spacing = 3
+        stackView.backgroundColor = UIColor(named: "grayMidBlur")
+        stackView.layer.cornerRadius = 10
+        
+        //esssa propiedade abaixo fala que dentro da stackView Vertical pode haver margens e ele seta as margens dentro dela de acordo com as stackViews horizontais.
+        
+        stackView.isLayoutMarginsRelativeArrangement = true
+        stackView.directionalLayoutMargins = NSDirectionalEdgeInsets(top: 20,
+                                                                     leading: 20,
+                                                                     bottom: 20,
+                                                                     trailing: 20)
+        
+        return stackView
+    }()
+    
+    
     
  // Métodos do Ciclo de Vida
     
@@ -106,16 +203,20 @@ class ViewController: UIViewController {
         setHierarchy()
         setConstraints()
         
-        
     }
+    
+    // Hierarquia de construção das telas
     
     private func setHierarchy() {
         
         view.addSubview(backGroundView)
         view.addSubview(headerView)
+        view.addSubview(statsStackView)
+        
         headerView.addSubview(cityLabel)
         headerView.addSubview(temperatureLabel)
         headerView.addSubview(weatherIcon)
+        
         
     }
     
@@ -175,6 +276,15 @@ class ViewController: UIViewController {
             weatherIcon.centerYAnchor.constraint(equalTo: temperatureLabel.centerYAnchor),
             
             ])
+        
+        
+        NSLayoutConstraint.activate([
+        
+            statsStackView.topAnchor.constraint(equalTo: headerView.bottomAnchor, constant: 24),
+            statsStackView.widthAnchor.constraint(equalToConstant: 206),
+            statsStackView.centerXAnchor.constraint(equalTo: view.centerXAnchor)
+        
+        ])
             
     }
     
